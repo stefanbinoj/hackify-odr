d5093fbc5e7351188b7c67470c2bf241f6b0cc44
stefanbinoj <stefan.binoj.007@gmail.com>
2025-03-01 08:47:37 +0530

    admin

---
+++ src/app/admin/component/component2.tsx
@@ -0,0 +1,5 @@
+const Component2 = () => {
+  return <div>Component2</div>
+}
+
+export default Component2
---
+++ src/app/admin/component/component3.tsx
@@ -0,0 +1,5 @@
+const Component3 = () => {
+  return <div>Component3</div>
+}
+
+export default Component3
